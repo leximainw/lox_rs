210a9cfe5d90730ce5f3cfa9d4b5eb69685b4f68
Alexis Mainwaring <leximainw@gmail.com>
2022-07-26 12:14:22 -0400

    check for script name input

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,28 @@
-use std::io;
+use std::{env, io};
 
 fn main()
+{
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 2
+    {
+        println!("usage: {} [script]", &args[0])
+    }
+    else if args.len() == 2
+    {
+        run_file(&args[1])
+    }
+    else
+    {
+        run_prompt()
+    }
+}
+
+fn run_file(_file_name: &String)
+{
+    println!("TODO")
+}
+
+fn run_prompt()
 {
     for line in io::stdin().lines()
     {
