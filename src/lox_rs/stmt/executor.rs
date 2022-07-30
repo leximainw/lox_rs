f49636c29ff56ec87ad71a853077be62a3e10e2e
Alexis Mainwaring <leximainw@gmail.com>
2022-07-29 20:14:07 -0400

    add behavior for ExprStmt

--- src/lox_rs/stmt/executor.rs
+++ src/lox_rs/stmt/executor.rs
@@ -5,7 +5,11 @@ impl Visitor<Result<(), (&'static str, (usize, usize))>> for AstExecutor
 {
     fn visit_exprstmt(stmt: &ExprStmt) -> Result<(), (&'static str, (usize, usize))>
     {
-        Ok(())
+        match stmt.expr.run()
+        {
+            Ok(_) => Ok(()),
+            Err(err) => Err(err)
+        }
     }
 
     fn visit_printstmt(stmt: &PrintStmt) -> Result<(), (&'static str, (usize, usize))>
